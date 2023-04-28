6a91710e5ecdf1e3d2219c7c1c8d33d0693e13af
Uli Rodriguez <fulizesraudales@gmail.com>
2023-04-27 20:21:30 -0700

    added more colors

--- Colors.h
+++ Colors.h
@@ -7,6 +7,21 @@ const WFM_Color WFM_RED = {1.0f, 0.0f, 0.0f, 1.0f};
 const WFM_Color WFM_GREEN = {0.0f, 1.0f, 0.0f, 1.0f};
 const WFM_Color WFM_BLUE = {0.0f, 0.0f, 1.0f, 1.0f};
 const WFM_Color WFM_BLACK = {0.0f, 0.0f, 0.0f, 1.0f};
+const WFM_Color WFM_WHITE = {1.0f, 1.0f, 1.0f, 1.0f};
+const WFM_Color WFM_YELLOW = {1.0f, 1.0f, 0.0f, 1.0f};
+const WFM_Color WFM_CYAN = {0.0f, 1.0f, 1.0f, 1.0f};
+const WFM_Color WFM_MAGENTA = {1.0f, 0.0f, 1.0f, 1.0f};
+const WFM_Color WFM_GRAY = {0.5f, 0.5f, 0.5f, 1.0f};
+const WFM_Color WFM_LIGHTGRAY = {0.75f, 0.75f, 0.75f, 1.0f};
+const WFM_Color WFM_DARKGRAY = {0.25f, 0.25f, 0.25f, 1.0f};
+const WFM_Color WFM_ORANGE = {1.0f, 0.5f, 0.0f, 1.0f};
+const WFM_Color WFM_BROWN = {0.5f, 0.25f, 0.0f, 1.0f};
+const WFM_Color WFM_PINK = {1.0f, 0.75f, 0.75f, 1.0f};
+const WFM_Color WFM_PURPLE = {0.5f, 0.0f, 0.5f, 1.0f};
+const WFM_Color WFM_GOLD = {1.0f, 0.85f, 0.0f, 1.0f};
+const WFM_Color WFM_SILVER = {0.75f, 0.75f, 0.75f, 1.0f};
+const WFM_Color WFM_BRONZE = {0.8f, 0.5f, 0.2f, 1.0f};
+const WFM_Color WFM_TRANSPARENT = {0.0f, 0.0f, 0.0f, 0.0f};
 // add more color constants as needed
 
 #endif // COLORS_H
