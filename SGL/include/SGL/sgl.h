a6933085252914bbec1c93fab285f75a25177f68
Uli Rodriguez <fulizesraudales@gmail.com>
2023-05-02 21:18:40 -0700

    redoing stuff

--- SGL/include/SGL/sgl.h
+++
@@ -1,8 +0,0 @@
-#ifndef SGL_H
-#define SGL_H
-
-#include "sgl_window.h"
-#include "sgl_errors.h"
-#include "sgl_colors.h"
-
-#endif // SGL_H 
---
+++ app/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   
+
+    return 0;
+}
