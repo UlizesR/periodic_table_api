13f33d615e77b46d1db249582d97e359191db366
Uli Rodriguez <fulizesraudales@gmail.com>
2023-05-04 16:36:42 -0700

    added structs

--- SGL/include/SGL/image/sgl_draw.h
+++ SGL/include/SGL/image/sgl_draw.h
@@ -6,9 +6,19 @@
 #ifndef SGL_DRAW_H
 #define SGL_DRAW_H
 
+typedef struct {
+    int x;
+    int y;
+} Point;
 
-void sgl_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color);
+typedef struct {
+    Point origin;
+    int w;
+    int h;
+} SGL_Rect;
 
-void sgl_draw_rect(uint32_t *pixels, size_t width, size_t height, size_t x, size_t y, size_t w, size_t h, uint32_t color);
+void sgl_fill(uint32_t *pixels, size_t pixel_w, size_t pixel_h, uint32_t color);
+
+void sgl_draw_rect(uint32_t *pixels, size_t pixel_width, size_t pixel_height, SGL_Rect rect, uint32_t color);
 
 #endif  // SGL_DRAW_H
