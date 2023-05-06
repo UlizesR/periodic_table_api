3f65739e26de7bd6fb468ac0a436e2818d363833
Uli Rodriguez <fulizesraudales@gmail.com>
2023-05-06 16:15:32 -0700

    redoing something

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
 #include <SGL/sgl.h>
 
-#define HEIGHT 800
-#define WIDTH 600
-
-#define COLS 8
-#define ROWS 6
-#define CELL_WIDTH (WIDTH / COLS)
-#define CELL_HEIGHT (HEIGHT / ROWS)
-
-static uint32_t pixels[HEIGHT * WIDTH];
 
 int main(int argc, char** argv)
 {
 
-    SGL_Pattern data = { pixels, WIDTH, HEIGHT, ROWS, COLS, CELL_WIDTH, CELL_HEIGHT, SGL_BLACK };
-    if (!checker_pattern(data)) return -1;
-    return 0;
 }
