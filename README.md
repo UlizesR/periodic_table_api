5b4fe77c4955fbfc4a62c898e71e23e3ee6ff6b6
Uli Rodriguez <fulizesraudales@gmail.com>
2023-05-25 01:00:01 -0700

    initial commit for api

---
+++ README.md
@@ -0,0 +1,16 @@
+# Periodic Table API
+
+## About
+
+    A Python FastAPI that returns data on a specific element in the periodic table
+
+### Tools
+
+* Python
+* FastAPI
+* Pandas
+
+## TODO
+
+1. Finish filling out csv file with data
+2. Add more endpoints to get more specific data
---
+++ periodic_table.py
@@ -0,0 +1,29 @@
+"""
+This is an API that will be used to access information about 
+Elements in the Periodic Table.
+"""
+
+from fastapi import FastAPI, HTTPException
+from typing import Union
+import pandas as pd
+
+app = FastAPI()
+
+df = pd.read_csv('Periodic_Table.csv')
+
+@app.get("/element/{element_identifier}")
+async def read_element(element_identifier: Union[int, str]):
+    # Check if the identifier is an integer (atomic number) or a string (element name)
+    if isinstance(element_identifier, int):
+        if element_identifier not in df['atomic_number'].values:
+            raise HTTPException(status_code=404, detail="Element not found: invalid atomic number")
+        element_data = df[df['atomic_number'] == element_identifier]
+    else:
+        element_data = df[df['element'] == element_identifier]
+
+    # If there's no such element, return a 404.
+    if element_data.empty:
+        raise HTTPException(status_code=404, detail="Element not found")
+
+    # Otherwise, return the element data as JSON.
+    return element_data.to_dict(orient='records')[0]
